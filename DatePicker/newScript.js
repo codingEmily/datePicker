376dbb5cb36db7927ef722ea96fe60fa1e073ffd
Emily <devemployed@gmail.com>
2025-06-10 23:24:50 -0400

    not all working yet, but I made good progress on knowing what to do next

--- DatePicker/newScript.js
+++ DatePicker/newScript.js
@@ -51,7 +51,7 @@ function changeMonth(value) {
 function populateWholeCalendar() {
     firstDayWeekday = resetCalendar()
 
-    dynamicallyGrowCalendar()
+    // dynamicallyGrowCalendar()
 
     fillPrevMonthSection()
     fillCurrentMonthSection(currentDaySelected)
@@ -86,6 +86,7 @@ function fillPrevMonthSection() {
 
 function fillCurrentMonthSection(currentDaySelected) {
     let currentMonthDay = 1
+    let numOfPrevToShow = (Number(firstDayWeekday)) - 1
     
     for (let i = 1; i <= getDaysInMonth(currentDaySelected); i++) {
         // the above loop continues to "length of month" EVEN PASS THE CURRENT HTML ELEMENTS
@@ -100,38 +101,19 @@ function fillCurrentMonthSection(currentDaySelected) {
         dateIdNumber++
         
     }
-    if (currentMonthDay < getDaysInMonth(currentDaySelected)) {
+    if (numOfPrevToShow + getDaysInMonth(currentDaySelected) > 35) {
         console.log("true")
-    //     for (let i = 0; i <= 6; i++) {
-    //     // calendarGrid.appendChild('button')
-    //     let tempExtraButton = document.createElement('button')
-    //     tempExtraButton.id = `pos-${dateIdNumber}`
-    //     calendarGrid.appendChild(tempExtraButton)
-
-    //     let button = document.createElement('button')
-    //     calendarGrid.appendChild(button)
-    // }
-
+        dynamicallyGrowCalendar(currentMonthDay)
     }
-
-        //     <button id = "pos-29" class = "date row-5 col-1 "></button>
-        // <button id = "pos-30" class = "date row-5 col-2"></button>
-        // <button id = "pos-31" class = "date row-5 col-3"></button>
-        // <button id = "pos-32" class = "date row-5 col-4"></button>
-        // <button id = "pos-33" class = "date row-5 col-5"></button>
-        // <button id = "pos-34" class = "date row-5 col-6"></button>
-        // <button id = "pos-35" class = "date row-5 col-7"></button>
 } 
 
 function fillNextMonthSection() {
-    // console.log(firstDayWeekday)
     // let numOfPrevToShow = firstDayWeekday - 1
     // let numOfDatesFilled = numOfPrevToShow + currentMonthLength
-
-    let daysLeft = 35 - dateIdNumber
+    // the above is TRASH CODE, broke the function, just wanna kow how/why it broke it
     let newMonthDay = 1
 
-    for (let i = dateIdNumber; i <= 35 ; i++) {
+    for (let i = dateIdNumber; i <= 35 ; i++) { // THIS LOOP HAS TO BE RE-WRITTEN to ACCOMODATE the DYNAMIC ASPECT of ADDING A ROW SOMETIMES
         let nextTempDate = document.querySelector(`#pos-${dateIdNumber}`)
 
         nextTempDate.innerText = newMonthDay
@@ -140,27 +122,41 @@ function fillNextMonthSection() {
     }
 }
 
-function dynamicallyGrowCalendar() {
-    // add ONE ROW of dates to the END, assign all the necessary ids/classes
-    //      calculate which dateIdNumber these should be at so they work same as the hardcoded dates >>>> JUST START AT 36 DUH!!! 
+///// SCRAP PAPER BELOW -->> Good idea to hardcode the "Extra row" and change display to "none" or "block" per action. Must move to functions next
 
-    // do NOT add inner Text, this is happen in regular fillCalendar functions
-    // these WILL NEEED TO BE REMOVED in the reset function
+let allExtraDatesForTestingPurposes = document.querySelectorAll('.hiddenDates');
+let arrayOfExtraDates = Array.from(allExtraDatesForTestingPurposes)
+console.log(typeof(arrayOfExtraDates))
 
-    let numOfPrevToShow = (Number(firstDayWeekday)) - 1
-    if (numOfPrevToShow + getDaysInMonth(currentDaySelected) > 35) {
-        for (let i = 0; i <= 6; i++) {
-            extraDateNumber = 1
-
-            let tempExtraButton = document.createElement('button')
-            tempExtraButton.id = `pos-${dateIdNumber + extraDateNumber}`
-            // tempExtraButton.innerText = dateIdNumber
-            calendarGrid.appendChild(tempExtraButton)
-
-            extraDateNumber++
-        }
-    }
+for (let i = 0; i < arrayOfExtraDates.length; i++) {
+    // console.log(arrayOfExtraDates[i])
+    // arrayOfExtraDates[i].style = "color: red"
+    // arrayOfExtraDates[i].style = "display: none" // probaby betteer to manually change display to "block" or "none"
+//    console.log(arrayOfExtraDates[i].classList.contains('hiddenDates')) // would have to overwrite the other css
 }
+
+
+// function dynamicallyGrowCalendar(currentMonthDay) {
+//     // add ONE ROW of dates to the END, assign all the necessary ids/classes
+//     //      calculate which dateIdNumber these should be at so they work same as the hardcoded dates >>>> JUST START AT 36 DUH!!! 
+
+//     // do NOT add inner Text, this is happen in regular fillCalendar functions
+//     // these WILL NEEED TO BE REMOVED in the reset function
+//     let numOfPrevToShow = (Number(firstDayWeekday)) - 1
+
+//     if (numOfPrevToShow + getDaysInMonth(currentDaySelected) > 35) {
+//         for (let i = dateIdNumber; i <= 42; i++) {
+
+//             let tempExtraButton = document.createElement('button')
+//             // tempExtraButton.id = `pos-${dateIdNumber}`
+//             // tempExtraButton.innerText = dateIdNumber
+//             calendarGrid.appendChild(tempExtraButton)
+
+//             currentMonthDay++
+//             dateIdNumber++
+//         }
+//     }
+// }
 
 function changeDate(date, daysToAdd, monthsToAdd, yearsToAdd) {
     let returnedDate
